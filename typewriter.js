706c584c3ab6195b101e9f3d2ba07df4c1f8a10f
Graeme Clark <grae0026@stud.kea.dk>
2020-02-11 20:01:28 +0100

    Add function to loop through characters

--- typewriter.js
+++ typewriter.js
@@ -1,1 +1,23 @@
 "use strict";
+window.addEventListener("DOMContentLoaded", init);
+
+// get text from document class 'typerwritten'
+const typeText = document.querySelector(".typewritten").textContent;
+
+// remove text from class
+const noText = (document.querySelector(".typewritten").textContent = "");
+
+// set counter to 0
+let count = 0;
+
+// looping function to add a character at a time
+function init() {
+  document.querySelector(".typewritten").textContent = typeText.substring(0, count);
+  if (typeText.length > count) {
+    count++;
+    setTimeout(init, 100);
+  }
+  console.log(count);
+}
+
+console.log(typeText);
